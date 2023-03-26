0a4827800c69b67dbb7389cbff7fa9503f8fafb4
markus sulg <markus7sulg@gmail.com>
2023-03-27 01:23:44 +0300

    World index caching and translation #20

--- pages/3_✨_World_Index_-_EE.py
+++ pages/3_✨_World_Index_-_EE.py
@@ -52,7 +52,7 @@ df = df.sort_values(by=["date"])
 def change_value(change_from, change_to, column):
     df.loc[df[column]==change_from, column] = change_to
 # Estonian categories and techniques
-change_value("Paintings", "Maal", "category")
+change_value("Painting", "Maal", "category")
 change_value("Mixed media", "Segatehnika", "category")
 change_value("Graphics", "Graafika", "category")
 change_value("Drawing", "Joonistus", "category")
@@ -63,18 +63,44 @@ change_value("Acrylic", "Akrüül", "technique")
 change_value("Gouache", "Guašš", "technique")
 change_value("Painting", "Maal", "technique")
 change_value("Watercolor", "Akvarell", "technique")
+change_value("Pastel", "Pastell", "technique")
 #change_value("Tempera", "Tempera", "technique")
 
 change_value("Aquatint", "Akvatinta", "technique")
-change_value("Linoleum", "Linool", "technique")
-change_value("Drawing", "Joonistus", "technique")
 change_value("Etching", "Etsing", "technique")
-change_value("Graphics", "Graafika", "technique")
-change_value("Mixed", "Segatehnika", "technique")
-change_value("Mixed technique", "Segatehnika", "technique")
+change_value("Linoleum", "Linool", "technique")
+change_value("Linocut", "Linool", "technique")
+change_value("Etching", "Etsing", "technique")
+change_value("Screenprint", "Siiditrükk", "technique")
+change_value("Silkscreen", "Siiditrükk", "technique")
+change_value("Serigraph", "Siiditrükk", "technique")
 change_value("Silk print", "Siiditrükk", "technique")
-change_value("Vitrography", "Vitrograafia", "technique")
-change_value("Wood cut", "Puugravüür", "technique")
+change_value("Lithograph", "Litograafia", "technique")
+change_value("Offset", "Litograafia", "technique")
+change_value("Drypoint", "Kuivnõel", "technique")
+change_value("Engraving", "Gravüür", "technique")
+change_value("Graphics", "Graafika", "technique")
+
+change_value("Drawing", "Joonistus", "technique")
+change_value("Ink", "Tint", "technique")
+change_value("Crayon", "Pastell", "technique")
+df.loc[df["technique"]=="Pastell", "category"] = "Maal"
+change_value("Pencil", "Pliiats", "technique")
+change_value("Pen", "Pliiats", "technique")
+change_value("Charcoal", "Süsi", "technique")
+change_value("Graphite", "Grafiit", "technique")
+
+change_value("Photograph", "Fotograaf", "technique")
+change_value("Sculpture", "Skulptuur", "technique")
+change_value("Collage", "Kollaaž", "technique")
+
+change_value("Mixed", "Segatehnika", "technique")
+
+# remove multi-word techniques
+df.loc[df["technique"].str.contains(" "), "category"] = ""
+
+#change_value("Vitrography", "Vitrograafia", "technique")
+#change_value("Wood cut", "Puugravüür", "technique")
 
 kanvas_logo = get_img_with_href('data/horisontal-BLACK.png', 'https://kanvas.ai', '200px')
 st.sidebar.markdown(kanvas_logo, unsafe_allow_html=True)
@@ -94,6 +120,71 @@ Andmed on kogutud Euroopa põhiliste galeriide avalike oksjoni tulemuste põhjal
 Kanvas.ai kunstiindeksist puuduvad oksjoniväline kunsti info kuid meil on plaan \nhakata koguma ka NFTKanvas.ai lehel müüdud NFT kunstimeediumi andmeid.
 
 Kunstiindeksi metoodika on praegu väljatöötamisel. Soovituste ja kommentaaridega saatke meile e-kiri info@kanvas.ai.
+''')
+
+# FIGURE - treemap covering categories, techniques and authors by volume and overbid TOP 5
+toc.subheader('Joonis - Kunsti müügid asukoha ja galerii järgi (Top 5 asukohta)')
+top_10_loc = list(df["loc"].value_counts().nlargest(5).index)
+
+df2 = df[df["loc"].isin(top_10_loc)]
+#df2 = df2[df2["author"].isin(top_authors)]
+
+
+table_data = create_table(df2, "src", list(df2["src"].unique()), calculate_volume=False, table_height=250)
+df2["yearly_performance"] = [table_data[table_data["Tehnika"] == x]["Iga-aastane kasv (%)"] for x in df2["src"]]
+
+df2 = df2.groupby(['loc', 'src']).agg({'end_price':['sum'], 'yearly_performance':['mean']})
+df2.columns = ['total_sales', 'yearly_performance']
+df2 = df2.reset_index()
+
+@st.cache_data
+def create_treemap_1():
+    fig = px.treemap(df2, path=[px.Constant("Asukohad"), 'loc', 'src'], values='total_sales',
+                      color='yearly_performance',
+                      color_continuous_scale='RdBu',
+                      range_color = (-20, df2['yearly_performance'].mean()),
+                      labels={
+                         "yearly_performance": "Aasta tootlus (%)",
+                         "total_sales": "Kogumüük",
+                         "author": "Autor",
+                      })
+    return fig
+fig = create_treemap_1()
+fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
+fig.update_traces(hovertemplate='<b>%{label} </b> <br> Kogumüük: %{value}<br> Aasta tootlus (%): %{color:.2f}',)
+st.plotly_chart(fig, use_container_width=True)
+create_paragraph('''Tekst
+''')
+
+# FIGURE - treemap covering categories, techniques and authors by volume and overbid OTHER
+toc.subheader('Joonis - Kunsti müügid asukoha ja galerii järgi (Teised asukohad)')
+
+df2 = df[~df["loc"].isin(top_10_loc)]
+
+table_data = create_table(df2, "src", list(df2["src"].unique()), calculate_volume=False, table_height=250)
+df2["yearly_performance"] = [table_data[table_data["Tehnika"] == x]["Iga-aastane kasv (%)"] for x in df2["src"]]
+
+df2 = df2.groupby(['loc', 'src']).agg({'end_price':['sum'], 'yearly_performance':['mean']})
+df2.columns = ['total_sales', 'yearly_performance']
+df2 = df2.reset_index()
+
+@st.cache_data
+def create_treemap_2():
+    fig = px.treemap(df2, path=[px.Constant("Asukohad"), 'loc', 'src'], values='total_sales',
+                      color='yearly_performance',
+                      color_continuous_scale='RdBu',
+                      range_color = (-20, df2['yearly_performance'].mean()),
+                      labels={
+                         "yearly_performance": "Aasta tootlus (%)",
+                         "total_sales": "Kogumüük",
+                         "author": "Autor",
+                      })
+    return fig
+fig = create_treemap_2()
+fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
+fig.update_traces(hovertemplate='<b>%{label} </b> <br> Kogumüük: %{value}<br> Aasta tootlus (%): %{color:.2f}',)
+st.plotly_chart(fig, use_container_width=True)
+create_paragraph('''...
 ''')
 
 prices = []
@@ -176,15 +267,19 @@ df2 = df2.groupby(['author', 'technique', 'category']).agg({'end_price':['sum'],
 df2.columns = ['total_sales', 'yearly_performance']
 df2 = df2.reset_index()
 
-fig = px.treemap(df2, path=[px.Constant("Techniques"), 'category', 'technique', 'author'], values='total_sales',
-                  color='yearly_performance',
-                  color_continuous_scale='RdBu',
-                  range_color = (-20, df2['yearly_performance'].mean()),
-                  labels={
-                     "yearly_performance": "Aasta tootlus (%)",
-                     "total_sales": "Kogumüük",
-                     "author": "Autor",
-                  })
+@st.cache_data
+def create_treemap_3():
+    fig = px.treemap(df2, path=[px.Constant("Tehnikad"), 'category', 'technique', 'author'], values='total_sales',
+                      color='yearly_performance',
+                      color_continuous_scale='RdBu',
+                      range_color = (-20, df2['yearly_performance'].mean()),
+                      labels={
+                         "yearly_performance": "Aasta tootlus (%)",
+                         "total_sales": "Kogumüük",
+                         "author": "Autor",
+                      })
+    return fig
+fig = create_treemap_3()
 fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
 fig.update_traces(hovertemplate='<b>%{label} </b> <br> Kogumüük: %{value}<br> Aasta tootlus (%): %{color:.2f}',)
 st.plotly_chart(fig, use_container_width=True)
@@ -196,65 +291,6 @@ toc.subheader('Tabel - Volüümi kasv Top 10 kunstnikul')
 table_data = create_table(df, "author", top_authors, calculate_volume=True, table_height=250)    
 st.table(table_data)
 create_paragraph('''Tekst
-''')
-
-# FIGURE - treemap covering categories, techniques and authors by volume and overbid
-toc.subheader('Joonis - Kunsti müügid asukoha ja galerii järgi (TOP 5)')
-top_10_loc = list(df["loc"].value_counts().nlargest(5).index)
-
-df2 = df[df["loc"].isin(top_10_loc)]
-#df2 = df2[df2["author"].isin(top_authors)]
-
-
-table_data = create_table(df2, "src", list(df2["src"].unique()), calculate_volume=False, table_height=250)
-df2["yearly_performance"] = [table_data[table_data["Tehnika"] == x]["Iga-aastane kasv (%)"] for x in df2["src"]]
-
-df2 = df2.groupby(['loc', 'src']).agg({'end_price':['sum'], 'yearly_performance':['mean']})
-df2.columns = ['total_sales', 'yearly_performance']
-df2 = df2.reset_index()
-
-fig = px.treemap(df2, path=[px.Constant("Locations"), 'loc', 'src'], values='total_sales',
-                  color='yearly_performance',
-                  color_continuous_scale='RdBu',
-                  range_color = (-20, df2['yearly_performance'].mean()),
-                  labels={
-                     "yearly_performance": "Aasta tootlus (%)",
-                     "total_sales": "Kogumüük",
-                     "author": "Autor",
-                  })
-fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
-fig.update_traces(hovertemplate='<b>%{label} </b> <br> Kogumüük: %{value}<br> Aasta tootlus (%): %{color:.2f}',)
-st.plotly_chart(fig, use_container_width=True)
-create_paragraph('''Tekst
-''')
-
-# FIGURE - treemap covering categories, techniques and authors by volume and overbid
-toc.subheader('Joonis - Kunsti müügid asukoha ja galerii järgi (Teised)')
-
-df2 = df[~df["loc"].isin(top_10_loc)]
-#df2 = df2[df2["author"].isin(top_authors)]
-
-
-table_data = create_table(df2, "src", list(df2["src"].unique()), calculate_volume=False, table_height=250)
-df2["yearly_performance"] = [table_data[table_data["Tehnika"] == x]["Iga-aastane kasv (%)"] for x in df2["src"]]
-
-df2 = df2.groupby(['loc', 'src']).agg({'end_price':['sum'], 'yearly_performance':['mean']})
-df2.columns = ['total_sales', 'yearly_performance']
-df2 = df2.reset_index()
-
-fig = px.treemap(df2, path=[px.Constant("Locations"), 'loc', 'src'], values='total_sales',
-                  color='yearly_performance',
-                  color_continuous_scale='RdBu',
-                  range_color = (-20, df2['yearly_performance'].mean()),
-                  labels={
-                     "yearly_performance": "Aasta tootlus (%)",
-                     "total_sales": "Kogumüük",
-                     "author": "Autor",
-                  })
-fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
-fig.update_traces(hovertemplate='<b>%{label} </b> <br> Kogumüük: %{value}<br> Aasta tootlus (%): %{color:.2f}',)
-st.plotly_chart(fig, use_container_width=True)
-create_paragraph('''...
 ''')
 
 def create_credits(text):
